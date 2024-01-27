f2f2e2a61a0412934877ad08015d10f6e7e1f3fc
Kenford20 <kzhou0066@gmail.com>
2024-01-27 03:27:27 -0600

    use negative assertion logic to make all routes public (to handle wedding website urls) except those specified in protectedRoutes of middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,10 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/api/trpc/website.find", "/websites(.*)"],
+  publicRoutes: (req) => {
+    const protectedRoutes = ["/dashboard", "/guest-list"];
+    return protectedRoutes.every((route) => !req.url.includes(route));
+  },
   ignoredRoutes: [],
   //   debug: true,
 });
