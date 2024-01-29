4b05a500614159aace20e0d532e1dd1cbfecbcc1
Kenford20 <kzhou0066@gmail.com>
2024-01-29 04:23:09 -0600

    add custom hook to help manage switching between different steps of the rsvp form

--- src/app/_components/hooks/index.ts
+++ src/app/_components/hooks/index.ts
@@ -1,4 +1,5 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { type ReactElement } from "react";
 
 const useDisablePageScroll = () => {
   useEffect(() => {
@@ -27,4 +28,37 @@ const useOuterClick = (callback: () => void) => {
   return ref;
 };
 
-export { useDisablePageScroll, useOuterClick };
+const useMultistepForm = (steps: ReactElement[]) => {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0);
+
+  function next() {
+    setCurrentStepIndex((i) => {
+      if (i >= steps.length - 1) return i;
+      return i + 1;
+    });
+  }
+
+  function back() {
+    setCurrentStepIndex((i) => {
+      if (i <= 0) return i;
+      return i - 1;
+    });
+  }
+
+  function goTo(index: number) {
+    setCurrentStepIndex(index);
+  }
+
+  return {
+    currentStepIndex,
+    formStep: steps[currentStepIndex],
+    formSteps: steps,
+    isFirstStep: currentStepIndex === 0,
+    isLastStep: currentStepIndex === steps.length - 1,
+    goTo,
+    next,
+    back,
+  };
+};
+
+export { useDisablePageScroll, useOuterClick, useMultistepForm };
