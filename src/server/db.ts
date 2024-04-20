e05c9c0451cbe8dbe25c6ea676c062508381c76c
Kenford20 <kzhou0066@gmail.com>
2024-04-19 20:37:38 -0500

    install postgres drivers for prisma

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,4 +1,6 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 import { env } from "~/env";
 
@@ -6,9 +8,14 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+const connectionString = `${env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
 export const db =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter,
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
